52292c78366a7cde3541f362ac19ae0146b82771
Sébastien Serre <sserre.bx@gmail.com>
2017-04-26 16:25:36 +0200

    cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,31 @@
 
 npm install --save-dev
 bower install
+
+echo "
+.PHONY: run test build build-preview
+
+run:
+	gulp serve
+
+test:
+	gulp serve:test
+
+build:
+	gulp
+
+build-preview: build
+	gulp serve:dist
+
+clean:
+	rm -rf dist
+
+clean_all: clean
+	rm -rf node_modules
+	rm -rf bower_components
+	rm -rf test/bower_components
+	rm -rf dist
+	rm -rf .tmp
+	rm -f Makefile
+
+" > Makefile
